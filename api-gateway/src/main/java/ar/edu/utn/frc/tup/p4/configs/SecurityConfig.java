60db9cd3d259f58a3d00ec2a18a1ad7500260da8
Rossi Braian Alexis <114080@tecnicatura.frc.utn.edu.ar>
2025-11-16 05:44:08 -0300

    Se agrega el microservicio user

--- api-gateway/src/main/java/ar/edu/utn/frc/tup/p4/configs/SecurityConfig.java
+++ api-gateway/src/main/java/ar/edu/utn/frc/tup/p4/configs/SecurityConfig.java
@@ -1,45 +1,24 @@
 package ar.edu.utn.frc.tup.p4.configs;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    @Value("${jwt.secret}")
-    private String jwtSecret;
-
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
-                        .anyExchange().permitAll()
+                        .pathMatchers("/api/v1/user/**").permitAll()
+                        .pathMatchers("/ping").permitAll()
+                        .anyExchange().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtDecoder(jwtDecoder())))
                 .build();
-    }
-
-
-    @Bean
-    public ReactiveJwtDecoder jwtDecoder() {
-        // Decodificar base64 si el secret contiene "=" (indicativo de Base64)
-        byte[] keyBytes = jwtSecret.contains("=")
-                ? Base64.getDecoder().decode(jwtSecret)
-                : jwtSecret.getBytes(StandardCharsets.UTF_8);
-
-        SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "HmacSHA256");
-        return NimbusReactiveJwtDecoder.withSecretKey(secretKey).build();
     }
 }
